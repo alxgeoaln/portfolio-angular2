4edf569528b47b92007e94c74163c68ca693b677
Georgian Alin Alexandru <georgianalinalexandru@gmail.com>
2017-09-27 17:31:11 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import {Component, HostBinding} from '@angular/core';
+import {trigger, transition, group, style, animate, query} from '@angular/animations';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('routeAnimation', [
+      transition('1 => 2', [
+        style({height: '!'}),
+        query(':enter', style({ transform: ''}))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  @HostBinding('@routeAnimation') routeAnimation = true;
   title = 'app works!';
 
   getDepth(outlet) {
